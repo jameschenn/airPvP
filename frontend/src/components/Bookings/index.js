e2ab0498e471962e1c4775962735a46a1f30eea7
James Chen <jameschen09@yahoo.com>
2022-07-05 20:34:39 -0700

    converted ISO-8601 date string

--- frontend/src/components/Bookings/index.js
+++ frontend/src/components/Bookings/index.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, Route, useParams } from 'react-router-dom';
+import moment from 'moment';
 import * as bookingActions from '../../store/bookings'
 
 const Bookings = () => {
@@ -21,7 +22,20 @@ const Bookings = () => {
     <>
     {bookingsData.map(booking => (
       <ul>
-        {booking.Spot.name}
+        <li>
+        <div>
+          <div>
+            <img src={booking?.Spot?.img1} alt={booking?.Spot?.name} />
+          </div>
+          <div>
+            {booking?.Spot?.name}
+            <br / >
+            {moment(booking?.startDate).utc().format('MMMM Do YYYY')}
+              &nbsp; to &nbsp;
+            {moment(booking?.endDate).utc().format('MMMM Do YYYY')}
+          </div>
+        </div>
+        </li>
       </ul>
     ))}
     </>
